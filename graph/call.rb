e555e370c20c9321cdc51c50760f1728c26318e2
ken <ken@example.com>
2014-02-09 12:02:51 +0900

    add transpose, iterators

--- graph/call.rb
+++ graph/call.rb
@@ -2,24 +2,26 @@
 # encoding: UTF-8
 require './graph.rb'
 require 'pp'
+to_key = (:to_i).to_proc
+adjs = DATA.readlines.map{|ln| ln.split.map(&to_key)
+}
+g = Graph.new(adjs, false)
 
-adjs = DATA.readlines.map{|ln| ln.split.map(&:to_sym) }
-g = Graph.new(adjs, true)
-puts g
-#p g.transpose
 =begin
 p g.tsort
+=end
 g.breadth_first_search(0)
 print 'path: '
 g.print_path(0, 15)
-=end
 __END__
-undershorts pants shoes
-socks shoes
-watch
-pants shoes belt
-shoes
-shirt belt tie
-belt jacket
-tie jacket
-jacket
+0 1 6 8
+1 2 3
+2 10 11
+3 4 12
+4 5 13
+5 6 9
+6 7
+7 8 9
+8 14
+9 15
+
--- graph/graph.rb
+++ graph/graph.rb
@@ -1,127 +1,153 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 require 'set'
+#require 'matrix' # for adj matrix
 
 class Fixnum
-#  def to_sym; to_i; end
+  def to_key; self; end
+end
+class String
+  def to_key; to_sym; end
 end
 class Graph
-  Vertex = Struct.new(:adjacency, :color, :pred, :discovered, :finished, :dist)
-  attr_reader :vertices
+  VertexItem = Struct.new(:color, :pred, :discovered, :finished, :dist)
+  attr_reader :adjacencies, :vertices_dict
 
-  def initialize(adjs, digraph = nil)
-    @vertices = Hash.new{|h, k| h[k] = Vertex.new([]) }
+  def initialize(adjs = {}, directed = true, klass = Array)
+    @vertices_dict = Hash.new{|h, k| h[k] = VertexItem.new }
+    @adjacencies = Hash.new{|h, k| h[k] = klass.new }
 
     adjs.each do |adj|
       v = adj.shift
-      @vertices[v].adjacency = adj
+      @adjacencies[v] = adj
+    end
+    unless directed
+      to_undirected!
     end
   end
+
   def each_vertex
-    @vertices.keys.each {|v| yield v }
+    @adjacencies.each_key {|v| yield v }
   end
+
   def each_edge
-    @vertices.each_pair do |u_id, u|
-      u.adjacency.each { |v| yield u_id, v }
+    @adjacencies.each_pair do |u, adj|
+      adj.each { |v| yield u, v }
     end
   end
-=begin
+
+  def add_vertex(v)
+    @adjacencies[v]
+  end
+
+  def add_edge(u, v)
+    add_vertex(v)
+    @adjacencies[u] << v
+  end
+
   def transpose
-    return self unless directed?
     g = self.class.new
-    each_vertex { |v| g.add_vertex v }
     each_edge { |u,v| g.add_edge(v, u) }
     g
   end
-=end
+
   def to_undirected
+    res = transpose
+    res.adjacencies.update(adjacencies){|key, self_v, other_v|
+      self_v | other_v }
+    res
   end
-  def add_edge(u, v)
-    @vertices[u].adjacency << v
-    add_vertex(v)
-  end
-  def add_vertex(v)
-    @vertices[v]
+  def to_undirected!
+    @adjacencies.replace to_undirected.adjacencies
+    self
   end
 
   def to_s
     str = ''
-    @vertices.each_pair do |k, v|
-      str += "#{k.to_sym}->#{v.adjacency} "
+    each_vertex do |v|
+      str += "#{v.to_key}->#{@adjacencies[v]} "
     end
     str.chop
-  end
-  def depth_first_search(s)
-    @vertices.each_value do |v|
-      v.discovered = v.finished = v.pred = nil
-      v.color = :White
-    end
-    @time = 0
-    dfs_visit(s)
-
-    vertices.each_pair do |v_id, v|
-      dfs_visit(v_id) if v.color == :White
-    end
-  end
-  def dfs_visit(u_id, before = nil, after = nil)
-    before && before[]
-    u = vertices[u_id]
-    u.color = :Gray
-    u.discovered = @time += 1
-
-    u.adjacency.each do |v_id|
-      v = vertices[v_id]
-      @acyclic = true if v.color == :Gray
-      if v.color == :White
-        v.pred = u_id
-        dfs_visit(v_id)
-      end
-    end
-    u.color = :Black
-    u.finished = @time += 1
-    after && after[]
-  end
-  def tsort
-    depth_first_search(@vertices.keys.first)
-    return false if @acyclic
-    @vertices.map{|k, v| [k, v.finished]}.sort_by{|v, k|
-      -k}.map(&:first) # 黒になったときlistの頭に加えたほうがよい
   end
 
   def print_path(s, v)
     if v == s
       print s
-    elsif vertices[v].pred.nil?
+    elsif @vertices_dict[v].pred.nil?
       print 'no-path'
     else
-      print_path(s, vertices[v].pred)
+      print_path(s, @vertices_dict[v].pred)
       print ' ', v
     end
   end
 
-  def breadth_first_search(s)
-    @vertices.each_value do |v|
-      v.pred = nil
-      v.dist = Float::MAX
-      v.color = :White
+  def depth_first_search(s)
+    each_vertex do |v|
+      v_it = @vertices_dict[v]
+      v_it.discovered = v_it.finished = v_it.pred = nil
+      v_it.color = :White
     end
-    @vertices[s].color = :Gray
+    @time = 0
+    dfs_visit(s)
+
+    each_vertex do |v|
+      dfs_visit(v) if @vertices_dict[v].color == :White
+    end
+  end
+
+  private
+  def dfs_visit(u, before = nil, after = nil)
+    before && before[]
+
+    u_it = @vertices_dict[u]
+    u_it.color = :Gray
+    u_it.discovered = @time += 1
+
+    @adjacencies[u].each do |v|
+      v_it = @vertices_dict[v]
+      @acyclic = true if v_it.color == :Gray
+      if v_it.color == :White
+        v_it.pred = u
+        dfs_visit(v)
+      end
+    end
+    u_it.color = :Black
+    u_it.finished = @time += 1
+    after && after[]
+  end
+
+  public
+  def tsort(s = @adjacencies.keys.first)
+    depth_first_search(s)
+    return false if @acyclic
+    @vertices_dict.map{|v, v_it| [v, v_it.finished]}.sort_by{|v_it, v|
+      -v}.map(&:first) # 黒になったときlistの頭に加えたほうがよい
+  end
+
+  def breadth_first_search(s)
+    each_vertex do |v|
+      v_it = @vertices_dict[v]
+      v_it.pred = nil
+      v_it.dist = Float::MAX
+      v_it.color = :White
+    end
+    @vertices_dict[s].color = :Gray
     queue = []
     queue << s
 
     until queue.empty?
-      u_id = queue.shift
-      u = @vertices[u_id]
-      u.adjacency.each do |v_id|
-        v = @vertices[v_id]
-        if v.color == :White
-          v.dist = u.dist + 1
-          v.pred = u_id
-          v.color = :Gray
-          queue << v_id
+      u = queue.shift
+      u_it = @vertices_dict[u]
+      @adjacencies[u].each do |v|
+        v_it = @vertices_dict[v]
+        if v_it.color == :White
+          v_it.dist = u_it.dist + 1
+          v_it.pred = u
+          v_it.color = :Gray
+          queue << v
         end
       end
-      u.color = :Black
+      u_it.color = :Black
     end
   end
 end # class Graph
