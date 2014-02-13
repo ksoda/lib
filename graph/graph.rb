04e1039ce0019c0f2d6403bc0d1a5b4a73490584
ken <ken@example.com>
2014-02-13 14:05:31 +0900

    add adj matrix

--- graph/graph.rb
+++ graph/graph.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
-require 'set'
-#require 'matrix' # for adj matrix
+require 'matrix' # for adj matrix
 DEBUG = nil
 
 class Fixnum
@@ -13,20 +12,39 @@ end
 
 class Graph
   VertexItem = Struct.new(:color, :pred, :discovered, :finished, :dist)
-  attr_reader :adjacencies, :vertices_dict
+  attr_reader :adjacencies, :vertices_dict, :directed
 
-  def initialize(adjs = {}, directed = true, klass = Array)
+  protected
+  attr_writer :directed
+
+  public
+  def initialize(adjs = [], options = {})
+    default = { directed: true, klass: Array, adj_mtx_size: false}
+    options = default.merge options
+
+    @adj_matrix_size = options[:adj_mtx_size]
+    @directed = options[:directed]
     @vertices_dict = Hash.new{|h, k| h[k] = VertexItem.new }
-    @adjacencies = Hash.new{|h, k| h[k] = klass.new }
 
-    adjs.each do |adj|
-      v = adj.shift
-      @adjacencies[v] = adj
+    unless @adj_matrix_size
+      # adj-list
+      @adjacencies = Hash.new{|h, k| h[k] = options[:klass].new }
+
+      adjs.each do |adj|
+        u = adj.shift
+        adj.each {|v| add_edge(u, v)}
+      end
+    else
+      # adj-matrix, index: zero start
+      # must not be index gap
+      @adjacencies = Array.new(@adj_matrix_size + 1){ [] }
+      adjs.each_with_index do |adj, ix|
+        u = adj.shift
+        adj.each {|v| add_edge(u, v)}
+      end
     end
-    unless directed
-      to_undirected!
-    end
-  end
+
+  end # initialize
 
   class VertexItem
     def inspect() "<#{color}:#{pred}:#{discovered}/#{finished}:#{dist}>"; end
@@ -34,39 +52,69 @@ class Graph
 
   def to_s
     str = ''
-    each_vertex do |v|
-      str += "#{v}->#{adjacencies[v]} "
+    unless @adj_matrix_size
+      each_vertex {|v| str += "#{v}->#{adjacencies[v]} " }
+    else
+      each_vertex do |v|
+        adj = adjacencies[v].each_with_index.
+          select{|e,i| e && e.nonzero?}.map{|pair| pair[1]}
+        str += "#{v}->#{adj} "
+      end
     end
     str.chop
   end
 
-  def print_path(s, v)
+  def make_path(s, v, path = [])
     if v == s
-      print s
+      path << s
     elsif vertices_dict[v].pred.nil?
-      print 'no-path'
+      raise 'no-path'
     else
-      print_path(s, vertices_dict[v].pred)
-      print ' ', v
+      make_path(s, vertices_dict[v].pred, path)
+      path << v
     end
   end
 
-
   def each_vertex
-    adjacencies.each_key {|v| yield v }
+    unless @adj_matrix_size
+      adjacencies.each_key {|v| yield v }
+    else
+      1.upto(@adj_matrix_size) {|v| yield v}
+    end
   end
 
   def each_edge
-    adjacencies.each_pair do |u, adj|
-      adj.each { |v| yield u, v }
+    unless @adj_matrix_size
+      adjacencies.each_pair do |u, adj|
+        adj.each { |v| yield u, v }
+      end
+    else
+      1.upto(@adj_matrix_size) do |u|
+        adjacencies[u].each {|v| yield u, v }
+      end
     end
   end
 
   def add_vertex(v) adjacencies[v]; end
 
   def add_edge(u, v)
-    add_vertex(v)
-    adjacencies[u] << v
+    unless @adj_matrix_size
+      add_vertex(v)
+      adjacencies[u] << v
+      adjacencies[v] << u unless @directed
+    else
+      adjacencies[u][v] = 1
+      adjacencies[v][u] = 1 unless @directed
+    end
+  end
+  def delete_edge(u, v)
+    unless @adj_matrix_size
+      adjacencies[u].delete(v)
+      adjacencies[v].delete(u) unless @directed
+    else
+      adjacencies[u][v] = 0
+      adjacencies[v][u] = 0 unless @directed
+    end
   end
 
   def transpose
@@ -79,11 +127,13 @@ class Graph
     res = transpose
     res.adjacencies.update(adjacencies){|key, self_v, other_v|
       self_v | other_v }
+    res.directed = false
     res
   end
 
   def to_undirected!
     adjacencies.replace to_undirected.adjacencies
+    self.directed = false
     self
   end
 
@@ -182,6 +232,26 @@ class Graph
     breadth_first_search
     vertices_dict.partition{|v, v_it| v_it.dist.even?}.
       map{|bl| bl.map(&:first)}
+  end
+
+  def init_single_source(s)
+    each_vertex do |v|
+      v_it = vertices_dict[v]
+      v_it.dist = Float::INFINITY
+      v_it.pred = nil
+    end
+    vertices_dict[s].dist = 0
+  end
+  def relax(u, v)
+    v_it, u_it = vertices_dict[v], vertices_dict[u]
+    weight = block_given? ? yield(u, v) : 1
+    if v_it.dist > u_it.dist + weight
+      v_it.dist = u_it.dist + weight
+      v_it.pred = u
+    end
+  end
+  def bellman_ford(s, &w)
+    init_single_source(s)
   end
 
 end # class Graph
