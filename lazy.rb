8c5a98cc1c984485131c77c59ab0f44623d6add5
ken <ken@example.com>
2014-03-04 12:08:42 +0900

    add astar

--- lazy.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# encoding: UTF-8
-# Higher order functions - you learn haskell
-p 100_000.downto(1).lazy.select {|i| i % 3829 == 0
-}.first
-
-p (1..Float::INFINITY).lazy.map{|i| i**2
-}.select(&:odd?).take_while{|i| i < 10_000
-}.inject(:+)
